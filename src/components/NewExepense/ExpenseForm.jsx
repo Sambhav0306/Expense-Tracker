04bf72af64368ea459f2c05d4d407f023bf7cd7e
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-09-29 19:05:02 +0530

    Form blank done

--- src/components/NewExepense/ExpenseForm.jsx
+++ src/components/NewExepense/ExpenseForm.jsx
@@ -22,20 +22,23 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         }
         console.log(expenseData);
+        setEnteredAmount('')
+        setEnteredDate('')
+        setEnteredTitle('')
     }
     return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className='new-expense__control'>
                 <label >Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className='new-expense__control'>
                 <label >Amount</label>
-                <input type='number' onChange={amountChangeHandler} />
+                <input type='number' value={enteredAmount} onChange={amountChangeHandler} />
             </div>
             <div className='new-expense__control'>
                 <label >Date</label>
-                <input type='date' min='2012-01-01' max="2023-12-31" onChange={dateChangeHandler} />
+                <input type='date' min='2012-01-01' max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
             </div>
         </div>
         <div className='new-expense__actions'>
